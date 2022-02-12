0b84f6e2cdbf1da5d0ee24597809b968a5efd69a
Inderpal Singh <inderpalsingh6763@gmail.com>
2022-02-12 17:10:43 -0500

    Added Tailwind Hero Icons

--- frontend/src/components/app/Sidebar.js
+++ frontend/src/components/app/Sidebar.js
@@ -8,6 +8,7 @@ import {
 import AppContext from "../../context/AppContext";
 import { CHANGE_SELECTED_TAB } from "../../reducers/reducerActions";
 import { Link } from "react-router-dom";
+import { DashboardIcon } from "@radix-ui/react-icons";
 
 const Sidebar = () => {
 	const { state, dispatch } = useContext(AppContext);
@@ -26,34 +27,94 @@ const Sidebar = () => {
 					<p className="font-medium text-xl">QuickMint</p>
 				</Link>
 			</div>
-			<div className="flex flex-col gap-y-2">
+			<div className="flex flex-col gap-y-2 text-base">
 				<div
 					className={`p-2 border-2 border-transparent ${
 						selectedTab === DASHBOARD_TAB ? "bg-bg-100" : "border-bg-100"
 					} rounded-lg hover:bg-bg-100 hover:cursor-pointer`}
 					onClick={() => handleSelectedTabChange(DASHBOARD_TAB)}>
-					{DASHBOARD_TAB}
+					<div className="flex justify-between items-center">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							className="h-6 w-6"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor">
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth={2}
+								d="M4 6h16M4 12h8m-8 6h16"
+							/>
+						</svg>
+						{DASHBOARD_TAB}
+					</div>
 				</div>
 				<div
 					className={`p-2 border-2 border-transparent ${
 						selectedTab === NFTS_GENERATOR_TAB ? "bg-bg-100" : "border-bg-100"
 					} rounded-lg hover:bg-bg-100 hover:cursor-pointer`}
 					onClick={() => handleSelectedTabChange(NFTS_GENERATOR_TAB)}>
-					{NFTS_GENERATOR_TAB}
+					<div className="flex justify-between items-center">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							className="h-6 w-6"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor">
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth={2}
+								d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"
+							/>
+						</svg>
+						{NFTS_GENERATOR_TAB}
+					</div>
 				</div>
 				<div
 					className={`p-2 border-2 border-transparent ${
 						selectedTab === SMART_CONTRACTS_TAB ? "bg-bg-100" : "border-bg-100"
 					} rounded-lg hover:bg-bg-100 hover:cursor-pointer`}
 					onClick={() => handleSelectedTabChange(SMART_CONTRACTS_TAB)}>
-					{SMART_CONTRACTS_TAB}
+					<div className="flex justify-between items-center">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							className="h-6 w-6"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor">
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth={2}
+								d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z"
+							/>
+						</svg>
+						{SMART_CONTRACTS_TAB}
+					</div>
 				</div>
 				<div
 					className={`p-2 border-2 border-transparent ${
 						selectedTab === ACCOUNT_TAB ? "bg-bg-100" : "border-bg-100"
 					} rounded-lg hover:bg-bg-100 hover:cursor-pointer`}
 					onClick={() => handleSelectedTabChange(ACCOUNT_TAB)}>
-					{ACCOUNT_TAB}
+					<div className="flex justify-between items-center">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							className="h-6 w-6"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor">
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth={2}
+								d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+							/>
+						</svg>
+						{ACCOUNT_TAB}
+					</div>
 				</div>
 			</div>
 		</div>
