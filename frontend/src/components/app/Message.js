a8890d5c0a1e9607ec00eeb3b13d806672620d9d
Inderpal Singh <inderpalsingh6763@gmail.com>
2022-02-12 17:53:45 -0500

    UI Fix for NFT Layers tab

--- frontend/src/components/app/Message.js
+++ frontend/src/components/app/Message.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-const Message = ({ message }) => {
+const Message = ({ message, classes }) => {
 	return (
-		<div className="bg-gray-50 w-full p-4 rounded-lg text-center font-medium ">
+		<div className={`bg-gray-50 w-full p-4 rounded-lg text-center font-medium ${classes}`}>
 			<p>{message}</p>
 		</div>
 	);
--- frontend/src/components/app/nft_generator/Layer.js
+++ frontend/src/components/app/nft_generator/Layer.js
@@ -33,55 +33,91 @@ const Layer = ({ id, layerName, layerImages }) => {
 	};
 
 	return (
-		<div className="p-6 w-full rounded-lg bg-gray-50 flex justify-between items-center">
-			<div className="w-full flex flex-col justify-center items-start gap-y-2">
+		<div className="p-6 w-full rounded-lg bg-gray-50 flex flex-col justify-between items-center gap-y-4">
+			<div className="w-full flex flex-col justify-center items-start">
 				{/* layer name */}
 				<div className="w-full flex justify-between items-center">
 					<h3 className="text-xl font-medium">{layerName}</h3>
-					<input
-						type="file"
-						name="images"
-						accept="image/png"
-						onChange={(e) => handleSelectedFiles(e)}
-						multiple
-					/>
+					<div>
+						<label htmlFor="layers" className="p-2 bg-gray-100 rounded-lg cursor-pointer hover:bg-gray-200">
+							Select Images
+							<input
+								type="file"
+								id="layers"
+								name="images"
+								accept="image/png"
+								onChange={(e) => handleSelectedFiles(e)}
+								multiple
+								className="bg-red-100"
+								style={{ display: "none" }}
+							/>
+						</label>
+					</div>
 				</div>
-				<div className="flex justify-center items-center gap-y-2 gap-x-4 flex-wrap p-4">
+			</div>
+			<div className="flex items-center bg-gray-100 rounded-lg w-full p-4">
+				<div className="flex justify-center items-center gap-y-4 gap-x-4 flex-wrap w-full">
 					{layerImages.length > 0 ? (
 						layerImages.map((image) => {
 							return (
-								<div key={image.id} className="flex flex-col gap-y-2 text-center">
+								<div
+									key={image.id}
+									className="flex flex-col justify-center items-center gap-y-2 text-center text-base">
 									<img src={image.src} height="150" width="150" alt={image.name} />
 									<div>
-										<p>{image.name}</p>
+										<p>{`${image.name.slice(0, 13)}...`}</p>
 									</div>
 								</div>
 							);
 						})
 					) : (
-						<Message message="No images uploaded." />
+						<Message message="No images uploaded." classes={"bg-gray-100 rounded-lg"} />
 					)}
 				</div>
-			</div>
 
-			{/* layer editing controls */}
+				{/* layer editing controls */}
 
-			<div className="flex gap-x-4">
-				<p
-					className="font-bold cursor-pointer"
-					onClick={() => {
-						dispatch({ type: CURRENT_LAYER_EDITING, payload: { id, layerName } });
-						dispatch({ type: TOGGLE_EDIT_LAYER_POPUP });
-					}}>
-					/
-				</p>
-				<p
-					className="font-bold cursor-pointer"
-					onClick={() => {
-						dispatch({ type: REMOVE_LAYER, payload: id });
-					}}>
-					X
-				</p>
+				<div className="flex gap-x-4">
+					<p
+						className="font-bold cursor-pointer"
+						onClick={() => {
+							dispatch({ type: CURRENT_LAYER_EDITING, payload: { id, layerName } });
+							dispatch({ type: TOGGLE_EDIT_LAYER_POPUP });
+						}}>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							className="h-6 w-6"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor">
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth={2}
+								d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+							/>
+						</svg>
+					</p>
+					<p
+						className="font-bold cursor-pointer"
+						onClick={() => {
+							dispatch({ type: REMOVE_LAYER, payload: id });
+						}}>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							className="h-6 w-6"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor">
+							<path
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth={2}
+								d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+							/>
+						</svg>
+					</p>
+				</div>
 			</div>
 		</div>
 	);
